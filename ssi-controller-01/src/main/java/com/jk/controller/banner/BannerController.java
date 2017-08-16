cb2c8f897a3d0ab4a164743f57db10573aefa2f0
wuxi <1050019299@qq.com>
2017-08-16 21:45:11 +0800

    提交控制

---
+++ ssi-controller-01/src/main/java/com/jk/controller/banner/BannerController.java
@@ -0,0 +1,14 @@
+package com.jk.controller.banner;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+@Controller
+public class BannerController {
+    public static void main(String[] args) {
+        System.out.println("猫");
+        System.out.println("11111");
+    }
+}
